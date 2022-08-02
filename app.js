c2b308efb659589e1a595469329c93d8fff66edc
gabrielakinola <gabrieltemitopeakinola@gmail.com>
2022-08-02 14:04:09 +0100

    Weather app

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const request = require("request");
 
-const url =
+const geocodeUrl =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/ibadan%20north.json?access_token=pk.eyJ1IjoiZ2FicmllbGFraW5vbGEiLCJhIjoiY2w0c2dhMWw0MGZ6YjNkbnVtYmR0aW0zMyJ9.ZPwGcG48Xpv9LtblciUYqQ&limit=1";
+
+request({ url: geocodeUrl, json: true }, (err, res) => {
+  if (err) {
+    console.log("Unable to connect to geocoding service");
+  } else {
+    const [longitude, latitude] = res.body.features[0].center;
+    console.log(latitude, longitude);
+  }
+});
+
+const weatherUrl =
   "http://api.weatherstack.com/current?access_key=1abca9ad80d041237953d9abaad6c215&query=7.469891,%203.911530&units=f";
 
-request({ url: url, json: true }, (error, response) => {
+request({ url: weatherUrl, json: true }, (error, response) => {
   const data = response.body.current;
-  //console.log(data.current);
+  console.log(data.current);
   console.log(
-    `The temperature is currently ${data.temperature}${response.body.request.unit}. It feels like ${data.feelslike}${response.body.request.unit} out there`
+    `${data.weather_descriptions[0]}. The temperature is currently ${data.temperature}${response.body.request.unit}. It feels like ${data.feelslike}${response.body.request.unit} out there`
   );
 });
