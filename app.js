17e25ca739a00ba1ed3bb812dd5e25bc8499bc64
gabrielakinola <gabrieltemitopeakinola@gmail.com>
2022-08-02 22:40:44 +0100

    Weather app

--- app.js
+++ app.js
@@ -5,20 +5,31 @@ const geocodeUrl =
 
 request({ url: geocodeUrl, json: true }, (err, res) => {
   if (err) {
-    console.log("Unable to connect to geocoding service");
+    console.log("Unable to connect to geocode api service");
+  } else if (res.body.features.length === 0) {
+    console.log("Unable to find location. Please try another search");
+    //console.log(res.body.features);
   } else {
     const [longitude, latitude] = res.body.features[0].center;
     console.log(latitude, longitude);
+    //console.log(res.body.features);
   }
 });
 
 const weatherUrl =
-  "http://api.weatherstack.com/current?access_key=1abca9ad80d041237953d9abaad6c215&query=7.469891,%203.911530&units=f";
+  "http://api.weatherstack.com/current?access_key=1abca9ad80d041237953d9abaad6c215&query=7.469891,%203.911530&units=m";
 
 request({ url: weatherUrl, json: true }, (error, response) => {
-  const data = response.body.current;
-  console.log(data.current);
-  console.log(
-    `${data.weather_descriptions[0]}. The temperature is currently ${data.temperature}${response.body.request.unit}. It feels like ${data.feelslike}${response.body.request.unit} out there`
-  );
+  if (error) {
+    console.log(`Unable to connect to weatherstack api`);
+  } else if (response.body.error) {
+    console.log(
+      "Please input the location coordinates using the querry string"
+    );
+  } else {
+    const data = response.body.current;
+    console.log(
+      `${data.weather_descriptions[0]}. The temperature is currently ${data.temperature} degree celcius. It feels like ${data.feelslike} degree celcius out there`
+    );
+  }
 });
